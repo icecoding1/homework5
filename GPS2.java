8a101cb42f099a6160f7a0a43a87fefcd19fdad4
pong dev beginer <patiphonpracticwork001@gmail.com>
2022-07-24 22:52:46 +0700

    add

--- GPS2.java
+++ GPS2.java
@@ -1,9 +1,8 @@
 import java.awt.*;
 import java.awt.event.*;
 
-
-class CalGPS extends Frame implements ActionListener {
-    String edu =  "1/2548";
+class GPS2 extends Frame implements ActionListener {
+    String edu = "1/2548";
     int Credits;
     double GPS;
     Button calButton = new Button("Calculate");
@@ -12,39 +11,41 @@ class CalGPS extends Frame implements ActionListener {
     Panel lPanel = new Panel();
     Panel rPanel = new Panel();
     Panel bPanel = new Panel();
-    String subject[] = {"Modern Programming", "Computer Graphics", "Project II", "Subject", "Subject", "Subject", "Subject", "Subject", "Subject", "Subject"};
-    String grad[] = {"A", "B+", "B", "C+", "C", "D+", "D", "F"};
+    String subject[] = { "Modern Programming", "Computer Graphics", "Project II", "Subject", "Subject", "Subject",
+            "Subject", "Subject", "Subject", "Subject" };
+    String grad[] = { "A", "B+", "B", "C+", "C", "D+", "D", "F" };
     Choice gradChoice = new Choice();
     TextField Semister = new TextField(edu + "");
     TextField Semister_cal = new TextField(edu + "");
     TextField Credits_cal = new TextField(Credits + "");
     TextField GPS_cal = new TextField(GPS + "");
-    public CalGPS() {
+
+    public GPS2() {
         super("JAVA Grade Point Semister");
-        tPanel.setLayout(new GridLayout(1,4));
+        tPanel.setLayout(new GridLayout(1, 4));
         add(tPanel, BorderLayout.NORTH);
         add(lPanel, BorderLayout.WEST);
-        //tPanel
+        // tPanel
         tPanel.add(new Label("Semister"));
         tPanel.add(Semister);
         tPanel.add(new Label(""));
         tPanel.add(new Label(""));
-        //lPanel
-        lPanel.setLayout(new GridLayout(13,3));
+        // lPanel
+        lPanel.setLayout(new GridLayout(13, 3));
         lPanel.add(new Label("Subject"));
         lPanel.add(new Label("Credit"));
         lPanel.add(new Label("Grade"));
-        for(int i = 0; i < subject.length; i++ ){
+        for (int i = 0; i < subject.length; i++) {
             lPanel.add(new TextField(subject[i]));
             lPanel.add(new TextField("0"));
             lPanel.add(gradChoice = new Choice());
-            for(int j = 0; j < grad.length; j++) {
+            for (int j = 0; j < grad.length; j++) {
                 gradChoice.addItem(grad[j]);
             }
         }
         // rPanel
         add(rPanel, BorderLayout.EAST);
-        rPanel.setLayout(new GridLayout(4,4));
+        rPanel.setLayout(new GridLayout(4, 4));
         rPanel.add(new Label("GPS Semister"));
         rPanel.add(Semister_cal);
         rPanel.add(new Label("Sum of Credits"));
@@ -60,10 +61,10 @@ class CalGPS extends Frame implements ActionListener {
         bPanel.add(new Label(" "));
         bPanel.add(new Label(" "));
         bPanel.add(calButton);
-        bPanel.add(aboutButton); 
+        bPanel.add(aboutButton);
         // program
         setVisible(true);
-        setSize(400,400);
+        setSize(400, 400);
         pack();
         CodeToHandleWinClose m = new CodeToHandleWinClose();
         addWindowListener(m);
@@ -72,40 +73,43 @@ class CalGPS extends Frame implements ActionListener {
     }
 
     public static void main(String[] args) {
-        new CalGPS();
+        new GPS2();
     }
-@Override
+
+    @Override
     public void actionPerformed(ActionEvent e) {
         // TODO Auto-generated method stub
-       if(e.getSource() == aboutButton) {
-        new About();
-       }
+        if (e.getSource() == aboutButton) {
+            new About();
+        }
     }
 }
 
 class About extends Frame implements ActionListener {
-        Button about_btn = new Button("OK");
-        Panel tPanel = new Panel();
-        Panel bPanel = new Panel();
-        public About() {
-            Dialog d = new Dialog(this, "About JAVA GPS");
-            d.setSize(200, 200);
-            d.setVisible(true);
-            d.add(tPanel, BorderLayout.CENTER);
-            d.add(bPanel, BorderLayout.SOUTH);
-            tPanel.add(new Label("My first GUI program"));
-            tPanel.add(new Label("java 1.4.2"));
-            bPanel.add(about_btn);
-            about_btn.addActionListener(this);
-            CodeToHandleWinClose m = new CodeToHandleWinClose();
-            d.addWindowListener(m);
+    Button about_btn = new Button("OK");
+    Panel tPanel = new Panel();
+    Panel bPanel = new Panel();
+
+    public About() {
+        Dialog d = new Dialog(this, "About JAVA GPS");
+        d.setSize(200, 200);
+        d.setVisible(true);
+        d.add(tPanel, BorderLayout.CENTER);
+        d.add(bPanel, BorderLayout.SOUTH);
+        tPanel.add(new Label("My first GUI program"));
+        tPanel.add(new Label("java 1.4.2"));
+        bPanel.add(about_btn);
+        about_btn.addActionListener(this);
+        CodeToHandleWinClose m = new CodeToHandleWinClose();
+        d.addWindowListener(m);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        if (e.getSource() == about_btn) {
+            setVisible(false);
         }
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            // TODO Auto-generated method stub
-           if(e.getSource() == about_btn) {
-                setVisible(false);
-           } 
-        }
-            
+    }
+
 };
